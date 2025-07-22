f7ca604bfa7e31b98a43fad6c26696c67a47bf2b
Richard Lalancette <richard.lalancette@gmail.com>
2025-07-21 20:44:03 -0400

    Implemented skill labels and icons toggling
    - Code polishing / Cleanup
    - Added UI configuration options

--- TitanWeaponSkills.lua
+++ TitanWeaponSkills.lua
@@ -5,7 +5,7 @@
 -- *
 -- * Titan Panel - Weapon Skills Addon
 -- * @Description: Displays the player's current weapon skills on Titan Panel
--- * @Version: 1.0.0
+-- * @Version: 1.1.0
 -- * @Date: Jul 19, 2025
 -- * @Author: Honour Bound Game Studios Inc.
 -- **************************************************************************
@@ -14,7 +14,7 @@
 local _G = getfenv(0);
 local ADDON_ID = "WeaponSkills" -- Short ID for the plugin
 local TITAN_BUTTON_NAME = "TitanPanel" .. ADDON_ID .. "Button" -- Full name of the Titan Panel button frame
-local VERSION = "1.0.0" -- Version of the addon
+local VERSION = "1.1.0" -- Version of the addon
 
 -- ****************************** Weapon Skill Types: ******************************
 -- Axes:                Used by classes like warriors, paladins, and rogues for melee combat.
@@ -53,6 +53,9 @@ local iconTable = {
                     Unarmed = "Interface\\Icons\\inv_gauntlets_30",
                     Wands = "Interface\\Icons\\inv_wand_06",
                 }
+                
+-- ******************************** Sound Effects *******************************
+local sfkIndex = 6518 -- WISP sound
 
 -- ******************************** Variables *******************************
 TitanWeaponSkillsSaved = {}
@@ -63,13 +66,11 @@ dbg:EnableDebug(false)
 dbg:EnableTopic("Events", true) 
 dbg:EnableTopic("Flow", true)
 
-
-
 -- ******************************** RegEvent *******************************
 ---local Register event if not already registered
 ---@param plugin 
 ---@param event
-local function RegEvent(plugin, event)
+local function RegisterEvent(plugin, event)
 	if not plugin:IsEventRegistered(event) then
 		plugin:RegisterEvent(event)
 	end
@@ -79,12 +80,14 @@ end
 ---@param action string
 ---@param reason string
 local function Events(action, reason)
-	local plugin = _G[TITAN_BUTTON_NAME]
+	local plugin = TitanUtils_GetPlugin(ADDON_ID);
 
 	if action == "register" then
-        RegEvent(plugin, "CHAT_MSG_SKILL")
+        RegisterEvent(plugin, "CHAT_MSG_SKILL")
+        RegisterEvent(plugin, "PLAYER_LEVEL_UP")
 	elseif action == "unregister" then
         plugin:UnregisterEvent("CHAT_MSG_SKILL")
+        plugin:UnregisterEvent("PLAYER_LEVEL_UP")
 	else
 		-- action unknown ???
 	end
@@ -109,23 +112,30 @@ local function OnLoad(self)
     self.registry = {
         id = ADDON_ID,
         category = "Combat",
-        version = "1.0",
+        version = VERSION,
         menuText = "Weapon Skills", -- Text displayed in the Titan Panel menu
+        menuTextFunction = PrepareWeaponSkillsMenu,
         tooltipTitle = "Weapon Skills", -- Title for the tooltip
-        buttonTextFunction = "GetButtonText", -- Function to get the text displayed on the button
-        tooltipTextFunction = "GetTooltipText", -- Function to generate the tooltip text
+        buttonTextFunction = GetButtonText, -- Function to get the text displayed on the button
+        tooltipTextFunction = GetTooltipText, -- Function to generate the tooltip text
         icon = "Interface\\Icons\\INV_Sword_27", -- Default icon for the button
         iconWidth = 16, -- Width of the icon
         notes = notes, -- Short description in the config
         controlVariables = {
             ShowIcon = true,
-            ShowLabelText = true,
-            DisplayOnRightSide = false
+            DisplayOnRightSide = false,
+            SkillIncreaseSoundNotification = true, -- Enable sound notification for skill increases
+            ShowSkillLabels = true, -- Show skill labels in the tooltip
+            ShowSkillIcons = true, -- Show skill icons in the tooltip
+            ShowSmallSkillIcons = true, -- Show small skill icons in the tooltip
         },
         savedVariables = {
             ShowIcon = 1,
-            ShowLabelText = 1,
-            DisplayOnRightSide = 0
+            DisplayOnRightSide = 0,
+            SkillIncreaseSoundNotification = 1,
+            ShowSkillLabels = 1, -- Default to showing skill labels
+            ShowSkillIcons = 1, -- Default to showing skill icons
+            ShowSmallSkillIcons = 1, -- Default to showing small skill icons
         }
     }
 end
@@ -136,96 +146,133 @@ end
 ---@param event string
 ---@param ... any
 local function OnEvent(self, event, ...)
-	local msg = "_OnEvent" .. " " .. tostring(event) .. ""
-	dbg:Out("Events", msg)
-    TitanPanelButton_UpdateButton(ADDON_ID)
+	dbg:Out("Events", "_OnEvent" .. " " .. tostring(event) .. "")
+
+    if event == "CHAT_MSG_SKILL" or "PLAYER_LEVEL_UP" then
+        PlaySound(sfkIndex)
+        TitanPanelButton_UpdateButton(ADDON_ID)
+    end
+end
+
+function isWeaponSkill(skillName)  
+    return skillName:find("Axe") or 
+           skillName:find("Bow") or
+           skillName:find("Crossbow") or
+           skillName:find("Dagger") or
+           skillName:find("Fist Weapons") or
+           skillName:find("Guns") or
+           skillName:find("Mace") or 
+           skillName:find("Polearm") or 
+           skillName:find("Staves") or 
+           skillName:find("Sword") or 
+           skillName:find("Thrown") or
+           skillName:find("Unarmed") or 
+           skillName:find("Wand")
+end
+
+-- ******************************** FormatSkillLevel *******************************
+---local Format the skill level text for display
+function FormatSkillRank(skillRank, skillMaxRank, verticalAlignment)
+    local currentSkillRankText = ""
+
+    currentSkillRankText = "" .. skillRank
+
+    -- Color the currentSkillText based on its value
+    if skillRank == skillMaxRank then
+        currentSkillRankText = "|cff00ff00" .. currentSkillRankText .. "|r" -- Green for maxed skill
+    elseif skillRank / skillMaxRank >= 0.90 then
+        currentSkillRankText = "|cffffff00" .. currentSkillRankText .. "|r" -- Yellow for medium skill
+    elseif skillRank / skillMaxRank >= 0.80 then
+        currentSkillRankText = "|cffffa500" .. currentSkillRankText .. "|r" -- Orange for high skill
+    else
+        currentSkillRankText = "|cffff0000" .. currentSkillRankText .. "|r" -- Red for low skill
+    end
+
+    -- Light grey for maxSkill
+    if skillRank == skillMaxRank then
+        currentSkillRankText = currentSkillRankText .. "|cff00ff00" .. "/" .. skillMaxRank .. "|r"
+    else
+        currentSkillRankText = currentSkillRankText .. "|cbbbbbbbb" .. "/" .. skillMaxRank .. "|r"
+    end
+
+    return currentSkillRankText
+end
+
+-- ******************************** FormatSkillIcon *******************************
+---local Format the skill icon for display
+function FormatSkillIcon(skillName, verticalAlignment)
+    local plugin = TitanUtils_GetPlugin(ADDON_ID)
+    local skillIcon = ""
+
+    -- Check if the plugin is configured to show skill icons
+    if plugin.controlVariables.ShowSkillIcons or verticalAlignment then
+
+        -- Check if the skill is a weapon skill and get the icon path
+        local skillNameWithoutDashOrSpaces = skillName:gsub("-", "_")
+        skillNameWithoutDashOrSpaces = skillNameWithoutDashOrSpaces:gsub(" ", "_")
+        local iconPath = iconTable[skillNameWithoutDashOrSpaces] or "Interface\\Icons\\INV_Sword_27" -- Default to sword icon if not found
+
+        -- If the skill is a weapon skill, prepend the icon to the skill name
+        if isWeaponSkill(skillName) then
+            if plugin.controlVariables.ShowLargeSkillIcons then
+                -- Use standard icon size
+                skillIcon = "|T" .. iconPath .. ":24:24:0:0|t "
+            else
+                -- Use small icon size if configured
+                skillIcon = "|T" .. iconPath .. ":16:16:0:0|t "
+            end
+        end
+    end
+
+    return skillIcon
+end
+
+-- ******************************** FormatSkillName *******************************
+---local Format the skill name for display
+function FormatSkillName(skillName, verticalAlignment)
+    local plugin = TitanUtils_GetPlugin(ADDON_ID)
+    local skillNameText = ""
+
+    if plugin.controlVariables.ShowSkillLabels or verticalAlignment then
+        skillNameText = "|cffffffff" .. skillName .. ": |r"
+    else
+        skillNameText = ""
+    end
+
+    return skillNameText
 end
 
 -- ******************************** GetWeaponSkillsList *******************************
 ---local Get the list of weapon skills and their current levels
 function GetWeaponSkillsList(verticalAlignment)
-    local allSkillsText = {}
+    local plugin = TitanUtils_GetPlugin(ADDON_ID)
+
+    local allSkillsTable = {}
     local numSkills = GetNumSkillLines()
 
     for skillIndex = 1, numSkills do
-        local skillName, isHeader, isExpanded, skillRank, numTempPoints, skillModifier, skillMaxRank, isAbandonable, stepCost, rankCost, minLevel, skillCostType, skillDescription = GetSkillLineInfo(skillIndex)
+        local skillName, _, _, skillRank, _, _, skillMaxRank, _, _, _, _, _, _ = GetSkillLineInfo(skillIndex)
 
-        local isWeaponSkill = 
-        skillName:find("Axe") or 
-        skillName:find("Bow") or
-        skillName:find("Crossbow") or
-        skillName:find("Dagger") or
-        skillName:find("Fist Weapons") or
-        skillName:find("Guns") or
-        skillName:find("Mace") or 
-        skillName:find("Polearm") or 
-        skillName:find("Staves") or 
-        skillName:find("Sword") or 
-        skillName:find("Thrown") or
-        skillName:find("Unarmed") or 
-        skillName:find("Wand")
-
-        if isWeaponSkill then
-            local currentSkill = skillRank
-            local maxSkill = skillMaxRank
-            local currentSkillText = "" .. currentSkill
-
-            -- Color the currentSkillText based on its value
-            if currentSkill == maxSkill then
-                currentSkillText = "|cff00ff00" .. currentSkillText .. "|r" -- Green for maxed skill
-            elseif currentSkill / maxSkill >= 0.90 then
-                currentSkillText = "|cffffff00" .. currentSkillText .. "|r" -- Yellow for medium skill
-            elseif currentSkill / maxSkill >= 0.80 then
-                currentSkillText = "|cffffa500" .. currentSkillText .. "|r" -- Orange for high skill
-            else
-                currentSkillText = "|cffff0000" .. currentSkillText .. "|r" -- Red for low skill
+        -- Check if the skill is a weapon skill and has a valid rank
+        if isWeaponSkill(skillName) and skillRank and skillRank > 0 then
+        
+            -- [Skill Icon] + [Skill Name] + ": " + [Skill Rank / Max Rank]
+            local skillIcon = FormatSkillIcon(skillName, verticalAlignment)
+            local skillNameText = FormatSkillName(skillName, verticalAlignment)
+            local skillRankText = FormatSkillRank(skillRank, skillMaxRank, verticalAlignment)
+            local separatorText = ""
+            
+            if verticalAlignment then
+                separatorText = "\t"
             end
-
-            -- Light grey for maxSkill
-            if currentSkill == maxSkill then
-                currentSkillText = currentSkillText .. "|cff00ff00" .. "/" .. maxSkill .. "|r"
-            else
-                currentSkillText = currentSkillText .. "|cbbbbbbbb" .. "/" .. maxSkill .. "|r"
-            end
-
-            local skillNameWithoutDashOrSpaces = skillName:gsub("-", "_")
-            skillNameWithoutDashOrSpaces = skillNameWithoutDashOrSpaces:gsub(" ", "_")
-            local iconPath = iconTable[skillNameWithoutDashOrSpaces] or "Interface\\Icons\\INV_Sword_27" -- Default to sword icon if not found
-            local skillIcon = "|T" .. iconPath .. ":24:24|t  "
-            local skillName = skillIcon .. skillName -- Prepend the icon to the skill name
-            skillName = "|cffffffff" .. skillName .. "|r" -- Default white for other skills
-
-            if currentSkill and maxSkill and currentSkill > 0 then
-                if verticalAlignment then
-                    tinsert(allSkillsText, skillName .. ": \t" .. currentSkillText)
-                else
-                    tinsert(allSkillsText, skillName .. ": " ..  currentSkillText)
-                end
-            end
+            
+            tinsert(allSkillsTable, skillIcon .. skillNameText .. separatorText .. skillRankText)
         end
     end
 
-    local combinedText = ""
+    local separator = verticalAlignment and "\n" or "   "
     
-    combinedText = allSkillsText
-    if #allSkillsText == 0 then
-        combinedText = "No weapon skills found."
-    else
-        -- If vertical alignment is requested, join with newline characters
-        if verticalAlignment then
-            combinedText = table.concat(allSkillsText, "\n")
-        else
-            -- Otherwise, join with commas for horizontal alignment
-            combinedText = table.concat(allSkillsText, "   ")
-        end
-    end
-
-    -- If vertical alignment is requested, prepend a newline character
-    if verticalAlignment then
-        combinedText = "\n" .. combinedText
-    end
-
-    return combinedText
+    return table.concat(allSkillsTable, separator)
 end
 
 
@@ -277,7 +324,7 @@ end
     
 -- ******************************** CreateTitanWeaponSkillsButton *******************************
 ---local Create the Titan Panel button for the Weapon Skills addon
-local function CreateTitanWeaponSkillsButton()
+local function CreateTitanButton()
     if _G[TITAN_BUTTON_NAME] then
         return -- If already created, do nothing
     end
@@ -287,23 +334,86 @@ local function CreateTitanWeaponSkillsButton()
     window:SetFrameStrata("FULLSCREEN")
     OnLoad(window)
 
-    window:SetScript("OnShow", function(self) 
-		OnShow(self);
-        TitanPanelButton_OnShow(self) 
-    end)
+    window:SetScript("OnShow", 
+        function(self) 
+		    OnShow(self);
+            TitanPanelButton_OnShow(self) 
+        end)
 
-    window:SetScript("OnHide", function(self) 
-		OnHide(self)
-    end)
+    window:SetScript("OnHide", 
+        function(self) 
+		    OnHide(self)
+        end)
+    
+    window:SetScript("OnEvent", 
+        function(self, event, ...) 
+            OnEvent(self, event, ...) 
+        end)
 
-    window:SetScript("OnEvent", function(self, event, ...) 
-        OnEvent(self, event, ...) 
-    end)
+	window:SetScript("OnClick", 
+	    function(self, button) 
+            OnClick(self, button);
+		    TitanPanelButton_OnClick(self, button);
+	    end)
+end
 
-	window:SetScript("OnClick", function(self, button) 
-        OnClick(self, button);
-		TitanPanelButton_OnClick(self, button);
-	end)
+--- Function to prepare the right-click menu for the addon.
+-- This function is referenced by the plugin's registry (menuTextFunction).
+function PrepareWeaponSkillsMenu()
+    local plugin = TitanUtils_GetPlugin(ADDON_ID);
+    TitanPanelRightClickMenu_AddTitle(TitanPlugins[ADDON_ID].menuText);
+    
+    --local debugToggleButton = {};
+    --debugToggleButton.text = "Toggle Debug Output";
+    --debugToggleButton.func = function()
+    --    dbg:EnableDebug(not dbg.enabled);
+    --    TitanPanelButton_UpdateButton(ADDON_ID);
+    --end
+    --
+    --debugToggleButton.checked = dbg.enabled; -- Checkmark if debugging is enabled
+    --TitanPanelRightClickMenu_AddButton(debugToggleButton);
+
+    -- ************************************** --
+    -- Skill Label Toggle
+    local displaySkillLabelsButton = {};
+    displaySkillLabelsButton.text = "Skill Labels";
+    displaySkillLabelsButton.func = function()
+        plugin.controlVariables.ShowSkillLabels = not plugin.controlVariables.ShowSkillLabels;
+        TitanPanelButton_UpdateButton(ADDON_ID);
+    end
+    
+    displaySkillLabelsButton.checked = plugin.controlVariables.ShowSkillLabels;
+    TitanPanelRightClickMenu_AddButton(displaySkillLabelsButton);
+
+    -- ************************************** --
+    -- Skill Icons Toggle
+    local displaySkillIconsButton = {};
+    displaySkillIconsButton.text = "Skill Icons";
+    displaySkillIconsButton.func = function()
+        plugin.controlVariables.ShowSkillIcons = not plugin.controlVariables.ShowSkillIcons;
+        TitanPanelButton_UpdateButton(ADDON_ID);
+    end
+
+    displaySkillIconsButton.checked = plugin.controlVariables.ShowSkillIcons;
+    TitanPanelRightClickMenu_AddButton(displaySkillIconsButton);
+    
+    -- ************************************** --
+    -- Large Skill Icons Toggle
+    local displayLargeSkillIcons = {};
+    displayLargeSkillIcons.text = "Large Skill Icons";
+    displayLargeSkillIcons.func = function()
+        plugin.controlVariables.ShowLargeSkillIcons = not plugin.controlVariables.ShowLargeSkillIcons;
+        TitanPanelButton_UpdateButton(ADDON_ID);
+    end
+    displayLargeSkillIcons.checked = plugin.controlVariables.ShowLargeSkillIcons;
+    TitanPanelRightClickMenu_AddButton(displayLargeSkillIcons);
+
+    -- ************************************** --
+    TitanPanelRightClickMenu_AddSpacer();
+    
+    -- ************************************** --
+    -- Default Titan Panel Options
+    TitanPanelRightClickMenu_AddControlVars(ADDON_ID);
 end
 
 -- ******************************** Initialization *******************************
@@ -311,7 +421,7 @@ end
 -- This ensures Titan Panel is loaded before we try to interact with it.
 if TITAN_ID then
     dbg:Out("Flow", "TitanWeaponSkills: TITAN_ID found. Attempting to create button frame.")
-    CreateTitanWeaponSkillsButton()
+    CreateTitanButton()
 else
     -- If TITAN_ID is not immediately available, we might still be too early.
     -- This scenario is less likely with ##Dependencies, but good to be aware.
